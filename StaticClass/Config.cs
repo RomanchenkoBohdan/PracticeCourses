9bd615f38e0fd3ca8dbaaa9f8e5cf720d277b0b1
Bohdan Romanchenko <rov4ak3@gmail.com>
2020-10-02 06:41:32 +0300

    Add project files.

---
+++ StaticClass/Config.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace StaticClass
+{
+    public static class Config
+    {
+        public static string Path{ get; set; }
+        public static string Text{ get; set; }
+        static Config()
+        {
+            Path = "default.txt";
+        }
+
+        public static void WriteLine()
+        {
+            if (Path != null)
+            {
+                using (StreamWriter file = new StreamWriter(Path, true))
+                {
+                    file.WriteLine(Text);
+                }
+            }
+            else
+            {
+                using (StreamWriter file = new StreamWriter("test.txt", true))
+                {
+                    file.WriteLine(Text);
+                }
+            }            
+
+            Console.WriteLine("Good job");
+        }
+
+        public static string ReadLine()
+        {
+            using (StreamReader streamReader = new StreamReader(Path, true))
+            {
+                var result = streamReader.ReadToEnd();
+                //Console.WriteLine(result);
+
+                return result;
+            }
+        }
+
+    }
+}
---
+++ StaticClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace StaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Config.Path = "test.txt";
+
+            var tesla = new Car() { Name = "Model S", Price = 65000, Count = 137 };
+            var ford = new Car() { Name = "Model T", Price = 165000, Count = 3 };
+            var BMW = new Car() { Name = "M 7", Price = 105000, Count = 33 };
+
+            List<Car> listCars = new List<Car>() { tesla, ford, BMW };
+
+            //string jsonCar = JsonConvert.SerializeObject(listCars);
+
+            //Config.Text = jsonCar;
+            //Config.WriteLine();
+
+            ////////////////////////////////////////
+
+            var newResource = JsonConvert.DeserializeObject<Car>(Config.ReadLine());
+            
+            Car audi = newResource;
+            audi.Name = "RS 6";
+            audi.Price = 63000;
+            audi.Count = 64;
+
+            listCars.Add(audi);
+            
+            foreach (var item in listCars)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
